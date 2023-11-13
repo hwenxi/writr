6aeedf577b999d559ccbbf040cc7cf69d33540a8
Wenxi Huang <wenxi.huang@grantstreet.com>
2023-11-13 15:12:46 -0500

    final nginx and docker configurations - it works

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -5,6 +5,9 @@ import { serializeNonPOJOs } from '$lib/utils';
 
 export const handle = async ({ event, resolve }) => {
 	event.locals.pb = new PocketBase('http://localhost:8090');
+	// Using nginx reverse proxy - use this instead of localhost
+	// event.locals.pb = new PocketBase('http://pocketbase:8090) 
+	
 	// This should auth with OIDC2 instead of whuang
 	const authData = await event.locals.pb.collection('users').authWithPassword(POCKETBASE_ADMIN_USER, POCKETBASE_ADMIN_PW);
 	//event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
